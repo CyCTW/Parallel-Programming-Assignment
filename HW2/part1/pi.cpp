8ec94a5d2cf302e39eb8fea8408aafd76aa4d917
cyctw <yuaannn_192.cs06@g2.nctu.edu.tw>
2020-10-26 16:07:17 +0000

    3sec

--- HW2/part1/pi.cpp
+++ HW2/part1/pi.cpp
@@ -1,9 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
 #include<time.h>
 #include<pthread.h>
 #include<string.h>
 #include<unistd.h>
+#include<random>
+#include<chrono>
 
 typedef long long lld;
 lld total_num_of_tosses;
@@ -11,22 +12,23 @@ int thread_num;
 lld in_circle[10] = { 0 };
 int part = 0;
 
+lld rand_max = 2147483647;
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_real_distribution<double> dist(-1,1);
+using namespace std;
 // thread exec function
 void* sub_func(void* arg) {
 	lld num_of_tosses = total_num_of_tosses / thread_num;
 	int n = part++;
 
 	for(lld toss = 0; toss<num_of_tosses; toss++) {
-		double x = 2.0 * rand() / double(RAND_MAX);
-		double y = 2.0 * rand() / double(RAND_MAX);
+		double x = dist(gen) ;
+		double y = dist(gen) ;
+		// double x = 2.0 * rand() / double(RAND_MAX);
+		// double y = 2.0 * rand() / double(RAND_MAX);
 
 		// rescale range from 0~2 to -1~1
-		x -= 1.0;
-		y -= 1.0;
-		if (x > 1.0 || y > 1.0) {
-			printf("ERROR!\n\n");
-			break;
-		}
 
 		// calculate distance from 0,0
 		double dis = x*x+y*y;
@@ -34,36 +36,32 @@ void* sub_func(void* arg) {
 			in_circle[n]++;
 		}
 	}
-	printf("num, in_circle, num_of_tosses: %d, %lld %lld\n", n, in_circle[n], num_of_tosses);
-	// pthread_exit((void *) &in_circle);
+	cout << "Thread_num: " << n << "in_circle: " << in_circle[n] << "num_of_tosses: " << num_of_tosses << '\n';
 }
 
 
 int main(int argc, char** argv) {
-	clock_t begin = clock();
-	srand( time(NULL) );
+	//srand( time(NULL) );
+
 
 	thread_num = atoi(argv[1]);
 	total_num_of_tosses = atoi(argv[2]);
-	// memset(in_circle, 0, sizeof(in_circle));
 
 	pthread_t pt[thread_num];
 	for(int i=0; i<thread_num; i++) {
 		pthread_create(&pt[i], NULL, sub_func, NULL);
 	}
-	lld total_in_circle = 0;
 	for(int i=0; i<thread_num; i++) {
-		void *res;
-		pthread_join(pt[i], &res);
+		pthread_join(pt[i], NULL);
 	}
+	
+	lld total_in_circle = 0;
 	for(int i=0; i<thread_num; i++) {
 		total_in_circle += in_circle[i];
 	}
 	double pi_ = 4 * total_in_circle / (double)total_num_of_tosses ;
 
-	clock_t end = clock();
-
-	printf("Estimated PI = %lf\n", pi_);
-	printf("Calculation time = %.3f sec.\n", (double)(end-begin) / CLOCKS_PER_SEC);
-	printf("RAND_MAX = %d\n", RAND_MAX);
+	cout <<"Estimated PI = " << pi_ << '\n';
+	cout << RAND_MAX << '\n';
+	cout << rand_max << '\n';
 }
