8a03a2ef3e8c5e3f63562ad698b37740f10fee19
cyctw <yuaannn_192.cs06@g2.nctu.edu.tw>
2020-11-15 02:57:57 +0000

    bfs

--- HW3/part2/breadth_first_search/bfs.cpp
+++ HW3/part2/breadth_first_search/bfs.cpp
@@ -5,12 +5,12 @@
 #include <string.h>
 #include <cstddef>
 #include <omp.h>
-
 #include "../common/CycleTimer.h"
 #include "../common/graph.h"
 
 #define ROOT_NODE_ID 0
 #define NOT_VISITED_MARKER -1
+// #define VERBOSE
 
 void vertex_set_clear(vertex_set *list)
 {
@@ -33,7 +33,7 @@ void top_down_step(
     vertex_set *new_frontier,
     int *distances)
 {
-    #pragma omp parallel for
+    // #pragma omp parallel for
     for (int i = 0; i < frontier->count; i++)
     {
 
@@ -111,42 +111,38 @@ void bfs_top_down(Graph graph, solution *sol)
 
 void bottom_up_step(Graph g,
     vertex_set *frontier,
-    vertex_set *new_frontier,
-    int *distances)
+    int *distances, 
+    int iteration)
 {
-
+    int local_count = 0;
+    #pragma omp parallel for reduction(+: local_count)
     for(int i = 0; i < g->num_nodes; i++) {
 
-        if (distances[i] == NOT_VISITED_MARKER) {
+        if (frontier->vertices[i] == NOT_VISITED_MARKER) {
             int start_edge = g->incoming_starts[i];
             int end_edge = (i == g->num_nodes - 1)
                            ? g->num_edges
                            : g->incoming_starts[i + 1];
 
-            bool find = false;
             // iterate all incoming edges
             for(int neighbor = start_edge; neighbor < end_edge; neighbor++) {
                 
                 int incoming = g->incoming_edges[neighbor];
 
-                for(int v = 0; v < frontier->count; v++) {
-                    int node = frontier->vertices[v];
+                // check sharing the same edge
+                if ( frontier->vertices[incoming] == iteration) {
+                    // add vertex i to new frontier
+                    distances[i] = distances[incoming] + 1;
+                    frontier->vertices[i] = iteration + 1;
+                    local_count += 1;
 
-                    // check sharing the same edge
-                    if (node == incoming) {
-                        // add vertex i to new frontier
-                        find = true;
-                        distances[i] = distances[node] + 1;
-                        int index = new_frontier->count++;
-                        new_frontier->vertices[index] = i;
-
-                        break;
-                    }
+                    break;
                 }
-                if (find) break;
             }   
         }
     }
+    
+    frontier->count = local_count;
 
 }
 void bfs_bottom_up(Graph graph, solution *sol)
@@ -162,42 +158,39 @@ void bfs_bottom_up(Graph graph, solution *sol)
     // As was done in the top-down case, you may wish to organize your
     // code by creating subroutine bottom_up_step() that is called in
     // each step of the BFS process.
+
     vertex_set list1;
-    vertex_set list2;
     vertex_set_init(&list1, graph->num_nodes);
-    vertex_set_init(&list2, graph->num_nodes);
 
     vertex_set *frontier = &list1;
-    vertex_set *new_frontier = &list2;
 
     // initialize all nodes to NOT_VISITED
-    for (int i = 0; i < graph->num_nodes; i++)
-        sol->distances[i] = NOT_VISITED_MARKER;
+    for (int i = 0; i < graph->num_nodes; i++){
+        sol->distances[i] = 0;
+        frontier->vertices[i] = NOT_VISITED_MARKER;
+    }
 
     // setup frontier with the root node
-    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
+    frontier->vertices[ROOT_NODE_ID] = 0; // store distance
+    frontier->count++;
     sol->distances[ROOT_NODE_ID] = 0;
+    int iteration = 0;
 
     while (frontier->count != 0)
     {
-
+        frontier->count = 0;
 #ifdef VERBOSE
         double start_time = CycleTimer::currentSeconds();
 #endif
 
-        vertex_set_clear(new_frontier);
-
-        bottom_up_step(graph, frontier, new_frontier, sol->distances);
+        bottom_up_step(graph, frontier, sol->distances, iteration);
+        iteration++;
 
 #ifdef VERBOSE
         double end_time = CycleTimer::currentSeconds();
         printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
 #endif
 
-        // swap pointers
-        vertex_set *tmp = frontier;
-        frontier = new_frontier;
-        new_frontier = tmp;
     }
 }
 
@@ -208,3 +201,4 @@ void bfs_hybrid(Graph graph, solution *sol)
     // You will need to implement the "hybrid" BFS here as
     // described in the handout.
 }
+
